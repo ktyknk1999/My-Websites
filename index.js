df536a1358e50b461b26409a7eb42440b8126fff
Kartikeya karnatak <49065700+ktyknk1999@users.noreply.github.com>
2021-07-21 19:46:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// var a = new Array(2);
+// a[0] = new Image();
+// a[0].src = "images/Name.png";
+
+// a[1] = new Image();
+// a[1].src = "images/Name2.png";
+
+// var i = 0;
+// function show() {
+
+//     document.getElementById("mn").src = a[i].src;
+//     i = i + 1;
+//     if (i > 1) {
+//         i = 0;
+//     }
+//     setTimeout("show();", 1000);
+
+// }
+
+// slideshow
+
+
+
